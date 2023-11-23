7454e33ed41a3a92c0f6b06affb591e5189f072b
JC <j.c.ashley4363@gmail.com>
2023-11-22 22:12:47 -0600

    connected front to back data returning

---
+++ src/App.jsx
@@ -0,0 +1,31 @@
+import api from './api/axiosConfig'
+import { useState, useEffect } from 'react'
+
+function App() {
+  const [movies, setMovies] = useState()
+
+  const getMovies = async () => {
+    try {
+      const response = await api.get("/api/v1/movies")
+      console.log(response.data)
+
+      setMovies(response.data)
+    } catch (error) {
+      console.error(error)
+
+    }
+
+
+  }
+
+  useEffect(() => {
+    getMovies();
+  }, [])
+
+  return (
+    <>
+    </>
+  )
+}
+
+export default App
